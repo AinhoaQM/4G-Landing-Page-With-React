e7ac2e9a6aeb39f44eb38f8c21fda3806a2444a9
AinhoaQM <ainhoaqm99@hotmail.com>
2023-04-17 18:30:54 +0000

    starting the exercise

---
+++ src/js/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div>
+			hola navbar
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ src/js/component/Web.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+
+const Web = () => {
+	return (
+		<div>
+			<Navbar/>
+			<Jumbotron/>
+			<Card/>
+		</div>
+	);
+};
+
+export default Web;
